996cb1265036738cbf5f9b7b602a4c35fd8a1acb
Leonardo <leonardo.xavier.clear@gmail.com>
2022-01-28 15:36:57 -0300

    Adicionando middlewares a aplicaçao

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: "Ler livro",
-    list: [{
-        _id: 1,
-        description: "Pagar fatura do cartão",
-        done: true
-    }, {
-        _id: 2,
-        description: "Reunião com equipe",
-        done: false
-    }, {
-        _id: 3,
-        description: "Consulta ao médico",
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -21,6 +9,8 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, description: action.payload }
         case 'TODO_SEARCHED':
             return { ...state , list: action.payload.data }
+        case 'TODO_ADDED':
+            return { ...state, description: ''}
         default:
             return state
     }
